8aa16c761134d6dddd2444f0eac7d13a60d05cff
PamdathePanda <Burkesvolleyball@gmail.com>
2023-09-20 21:44:42 -0600

    Prep 3 Done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+        int inputNumber = 0;
+
+        while (inputNumber != magicNumber)
+        {
+            Console.WriteLine("Whats the Magic Number?");
+            string input = Console.ReadLine();
+            inputNumber = int.Parse(input);
+            if (inputNumber != magicNumber)
+            {
+                if (inputNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                if (inputNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+        }
+        Console.WriteLine($"{magicNumber}, your Number {inputNumber}");
+        Console.WriteLine("you got it Right! Nice Job!");
+
     }
 }
