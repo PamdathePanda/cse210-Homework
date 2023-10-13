eea78b7aa7065687a0f0c2b2633253a46f5eea33
PamdathePanda <Burkesvolleyball@gmail.com>
2023-10-12 20:58:32 -0600

    learning 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction(1, 1);
+        Fraction wholeNumer = new Fraction(10);
+        Fraction empty = new Fraction();
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(wholeNumer.GetFractionString());
+        Console.WriteLine(empty.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
+        Console.WriteLine(wholeNumer.GetDecimalValue());
+        Console.WriteLine(empty.GetDecimalValue());
+
+
     }
 }
